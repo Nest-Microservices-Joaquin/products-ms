f49ee9cff2c902736ac279ac4fca08daa3f39a14
kamaleon19 <joaco.px@gmail.com>
2024-12-12 09:48:45 -0300

    custom filter actualizado

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   const logger = new Logger('Main')
   
-  console.log(envs.natsServers);
+
   
   const app = await NestFactory.createMicroservice<MicroserviceOptions>( // createMicroservice recibe como primer argumento el AppModule y luego un objeto con el tipo de transporte que usemos y sus respectivas opciones.
     AppModule,
